bf13fe92dfd2432c6799de033e9976a33d13b26a
DevBasem <engr.basemahmed@gmail.com>
2023-09-18 14:26:28 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -8,33 +8,24 @@
 int _printf(const char *format, ...)
 {
 	int char_count = 0;
-	int percent_flag = 0;
 	va_list args;
 
 	if (format == NULL)
 	{
-		return (-1); 
+		return (-1);
 	}
+
 	va_start(args, format);
+
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			if (percent_flag)
-			{
-				print_char('%', &char_count);
-				percent_flag = 0;
-			} 
-			else
-			{
-				handle_format_specifier(&format, args, &char_count);
-				percent_flag = 1;
-			}
+			handle_format_specifier(&format, args, &char_count);
 		}
 		else
 		{
 			print_char(*format, &char_count);
-			percent_flag = 0;
 		}
 		format++;
 	}
